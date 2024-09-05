b750261c4025b0f4f984db1f95ef21c9d951edde
Agustin-Klei <agusdel_94@hotmail.com>
2024-09-05 16:22:02 -0700

    Add WIP victory condition

--- code/game/final_level.gd
+++ code/game/final_level.gd
@@ -6,10 +6,18 @@ var player
 @export var fixed_speed_camera: Node2D
 @export var kaiju: Node2D
 
+@export var end_area : Area2D
+
 @export var game_over_panel: Control
-@export var retry_button: Button
-@export var quit_button: Button
+@export var game_over_retry_button: Button
+@export var game_over_quit_button: Button
+
+@export var victory_panel: Control
+@export var victory_quit_button: Button
+
 @export var quit_scene: PackedScene
+
+@export var skip_intro : bool = false
 
 func _ready():
     _run()
@@ -23,39 +31,42 @@ func _run():
     player = get_tree().get_first_node_in_group("Player")
     player.set_block_input(true)
 
-    await _wait(1)
-    kaiju.grow1()
-    _kaiju_say("NOM NOM NOM", 20)
+    if not skip_intro:
+        await _wait(1)
+        kaiju.grow1()
+        _kaiju_say("NOM NOM NOM", 20)
 
-    await _wait(2)
-    player.set_direction(-1) ## Face left
-    _player_say("Kaiju, what are you doing!?")
+        await _wait(2)
+        player.set_direction(-1) ## Face left
+        _player_say("Kaiju, what are you doing!?")
 
-    await _wait(2)
-    kaiju.grow2()
-    _kaiju_say("NOM NOM NOM", 45, Vector2(50, -200))
+        await _wait(2)
+        kaiju.grow2()
+        _kaiju_say("NOM NOM NOM", 45, Vector2(50, -200))
 
-    await _wait(2)
-    _player_say("You have to stop! Please, Kaiju!")
+        await _wait(2)
+        _player_say("You have to stop! Please, Kaiju!")
 
-    await _wait(2)
-    _kaiju_say("RAAAAARRRR", 60, Vector2(0, -100))
+        await _wait(2)
+        _kaiju_say("RAAAAARRRR", 60, Vector2(0, -100))
 
-    await _wait(2)
-    _player_say("Kaiju, please! It's me!")
+        await _wait(2)
+        _player_say("Kaiju, please! It's me!")
 
-    await _wait(2)
-    _kaiju_say("RAAAAAAAAAAAAAAAAAAARRRR", 80, Vector2(0, -50))
+        await _wait(2)
+        _kaiju_say("RAAAAAAAAAAAAAAAAAAARRRR", 80, Vector2(0, -50))
 
-    await _wait(2)
-    _player_say("Oh no!")
+        await _wait(2)
+        _player_say("Oh no!")
 
-    await _wait(1)
+        await _wait(1)
+    else: ## Skip. go straight to max size
+        kaiju.grow2()
+
     _player_say("")
     player.set_block_input(false)
     fixed_speed_camera.start_moving()
     kaiju.start_chasing()
-
 
 
 func _init_level():
@@ -74,6 +85,8 @@ func _init_level():
 
     kaiju.connect("on_catch_player", _on_kaiju_catch_player)
 
+    end_area.connect("body_entered", _on_player_escaped)
+
 func _on_kaiju_catch_player(_p: Node2D):
     player.set_block_input(true)
     fixed_speed_camera.stop_moving()
@@ -82,16 +95,53 @@ func _on_kaiju_catch_player(_p: Node2D):
     await _wait(3)
 
     game_over_panel.visible = true
-    game_over_panel.mouse_filter = Control.MOUSE_FILTER_STOP
-    retry_button.mouse_filter = Control.MOUSE_FILTER_STOP
-    quit_button.mouse_filter = Control.MOUSE_FILTER_STOP
-    retry_button.connect("pressed", _on_retry_pressed)
-    quit_button.connect("pressed", _on_quit_pressed)
+    game_over_retry_button.connect("pressed", _on_retry_pressed)
+    game_over_quit_button.connect("pressed", _on_quit_pressed)
+
+func _on_player_escaped(_p: Node2D):
+    if not player.is_in_group("Player"):
+        return
+
+    fixed_speed_camera.stop_moving()
+
+    await _wait(1)
+
+    kaiju.stop()
+
+    await _wait(0.5)
+
+    player.set_block_input(true)
+
+    await _wait(1)
+    _kaiju_say("RAAAAAAAAAAAAAAAAAAARRRR", 80)
+
+    await _wait(4)
+    _kaiju_say("", 80)
+    kaiju.retreat()
+
+    player.set_direction(-1) ## Face left
+
+    await _wait(3)
+    _player_say("Farewell... my friend.")
+
+    await _wait(5)
+    _player_say("")
+
+    var fade = get_tree().get_first_node_in_group("Fade")
+    fade.set_fade_time(3)
+    fade.fade_out()
+
+    await _wait(5)
+    victory_panel.visible = true
+    victory_quit_button.connect("pressed", _on_quit_pressed)
+
 
 func _on_retry_pressed():
+    player.queue_free()
     get_tree().reload_current_scene()
 
 func _on_quit_pressed():
+    player.queue_free()
     get_tree().change_scene_to_packed(quit_scene)
 
 func _player_say(text: String):
--- code/game/kaiju_full.gd
+++ code/game/kaiju_full.gd
@@ -7,27 +7,32 @@ extends CharacterBody2D
 signal on_catch_player
 
 var is_chasing : bool = false
+var is_retreating : bool = false
 
 func _ready():
     pass
 
 
 func _physics_process(delta):
-    if not is_chasing:
+    if not is_chasing and not is_retreating:
         return
 
-    velocity.x = speed
+    if is_chasing:
+        velocity.x = speed
+    else:
+        velocity.x = -speed
 
     move_and_slide()
 
-    var cols = get_slide_collision_count()
+    if is_chasing:
+        var cols = get_slide_collision_count()
 
-    for i in cols:
-        var obj = get_slide_collision(i)
-        var col = obj.get_collider()
+        for i in cols:
+            var obj = get_slide_collision(i)
+            var col = obj.get_collider()
 
-        if col.is_in_group("Player"):
-            _on_catch_player(col)
+            if col.is_in_group("Player"):
+                _on_catch_player(col)
 
 func _on_catch_player(player: Node2D):
     is_chasing = false
@@ -36,6 +41,15 @@ func _on_catch_player(player: Node2D):
 
 func start_chasing():
     is_chasing = true
+
+func retreat():
+    is_chasing = false
+    is_retreating = true
+
+func stop():
+    is_chasing = false
+    is_retreating = false
+
 
 func grow1():
     anim_player.play("Grow1")

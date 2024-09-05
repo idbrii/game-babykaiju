ab29050d29139792ed496a767635847b58029628
Agustin-Klei <agusdel_94@hotmail.com>
2024-09-05 15:38:21 -0700

    Main manu takes you to level_master now. Using path, otherwise theres a circular dependency

--- code/main_menu/main_menu.gd
+++ code/main_menu/main_menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
 @export var play_button : Button
-@export var game_scene : PackedScene
+@export_file("*.tscn") var game_scene
 
 func _ready():
     play_button.connect("pressed", _on_play_pressed)
 
 func _on_play_pressed():
-    get_tree().change_scene_to_packed(game_scene)
+    get_tree().change_scene_to_file(game_scene)

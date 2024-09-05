028acfff25559fa38d125fb886694d0aa2d7ccf1
David Briscoe <idbrii@gmail.com>
2024-09-05 12:04:06 -0700

    Make baby kaiju face your direction when held
    Position baby in a fixed holding position.
    Layer player arms on top of baby.
    Baby faces the direction you're facing.
    
    Retune throwing params since it all feels different with this new logic.
    Previously, I was using direction from player to kaiju for impulse, but
    now it's a fixed value, so it completely changed tuning.
    
    Also adjust player riding teen to jump off correctly.

--- code/platformer/grabbable.gd
+++ code/platformer/grabbable.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @export_range(0, 10000) var drop_impulse := 10.0
 @export_range(0, 10000) var drop_uplift := 5.0
+@export var _facing_node : Node2D
 
 @onready var _body = get_parent()
 @onready var original_parent = _body.get_parent()
@@ -10,6 +11,7 @@ extends Node2D
 var is_held := false
 var hold_offset := Vector2.ZERO
 var target_marker : Node2D
+var current_holder : CharacterBody2D
 
 
 func acquire(holder: PhysicsBody2D):
@@ -17,28 +19,40 @@ func acquire(holder: PhysicsBody2D):
     if _body is RigidBody2D:
         _body.freeze = true
     _body.add_collision_exception_with(holder)
-    _body.reparent(holder)
+    _body.reparent(holder.get_node("%grab_child_marker"))  # drawn before arms
     hold_offset = _body.global_position - holder.global_position
     target_marker = holder.get_hold_marker()
+    current_holder = holder
 
 
 func release(holder: PhysicsBody2D):
     is_held = false
+    var facing = current_holder.face_direction
     target_marker = null
-    _body.remove_collision_exception_with(holder)
+    current_holder = null
     _body.reparent(original_parent)
     if _body is RigidBody2D:
         _body.freeze = false
     await get_tree().process_frame
-    var dir = _body.global_position - holder.global_position
-    dir.normalized()
+
+    var throw_impulse = drop_impulse
+
+    var dir : Vector2
+    if holder.velocity.length_squared() > 0.1:
+        dir = holder.velocity
+    else:
+        # Not moving, so use facing direction.
+        dir = Vector2.RIGHT * facing
+        # Less motion when standing
+        throw_impulse *= 0.5
+    dir = dir.normalized()
 
     # Boost height to toss higher when jumping.
     var velocity_boost = 1
     if holder.velocity.y < 0:
         velocity_boost = 3
 
-    var forward = dir * drop_impulse
+    var forward = dir * throw_impulse
     var uplift = Vector2.UP * drop_uplift * velocity_boost
     var impulse = forward + uplift
     if _body is RigidBody2D:
@@ -46,9 +60,11 @@ func release(holder: PhysicsBody2D):
     elif _body is CharacterBody2D:
         _body.velocity = impulse
 
+    await get_tree().create_timer(0.75).timeout
+    _body.remove_collision_exception_with(holder)
+
 
 func _physics_process(_dt):
     if is_held:
-        _body.position = hold_offset
-        # TODO: How can we attach them to the arms instead?
-        #~ _body.global_position = target_marker.global_position
+        _body.global_position = target_marker.global_position
+        _facing_node.scale.x = current_holder.face_direction

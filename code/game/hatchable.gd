1a0050e2a750d6b5096e916a1bc6afaab2b8f3e1
David Briscoe <idbrii@gmail.com>
2024-09-05 17:14:46 -0700

    Egg hatches and jumps away

---
+++ code/game/hatchable.gd
@@ -0,0 +1,40 @@
+class_name Hatchable
+extends Area2D
+
+const KaijuBaby = preload("res://scenes/kaiju_baby.tscn")
+
+@export var throw_power := 1500.0
+@onready var _sprite_animator := $AnimatedSprite2D
+
+
+func _ready():
+    self.body_entered.connect(_on_body_entered)
+    _sprite_animator.pause()
+
+
+func _on_body_entered(toucher):
+    if not toucher.is_in_group("Player"):
+        return
+
+    toucher.set_block_input(true)
+
+    await get_tree().process_frame
+    monitoring = false
+
+    _sprite_animator.play("egg")
+    await get_tree().create_timer(1).timeout
+
+    visible = false
+    var baby = KaijuBaby.instantiate()
+    get_parent().add_child(baby)
+    baby.add_collision_exception_with(toucher)
+    baby.global_position = global_position
+
+    var throw = global_position.direction_to($ThrowMarker.global_position) * throw_power
+    baby.apply_central_impulse(throw)
+
+    await get_tree().create_timer(1).timeout
+
+    toucher.set_block_input(false)
+    baby.remove_collision_exception_with(toucher)
+    queue_free()
